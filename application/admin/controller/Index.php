58633662b0710063cec8c9fff2d5075b3d2d012b
shiroo <xkq722@qq.com>
2018-10-18 17:50:23 +0800

    上传功能移至File类

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -13,27 +13,7 @@ class Index extends Base
     public function index(){
         return 'welcome!';
     }
-    public function uploadFile(){
-        if(IS_POST){
-            $file=$this->request->file('file');
-            if(empty($file)){
-                return '没有获取到上传文件。';
-            }
-            $info=$file->validate([
-                'size'=>1048576,
-                'ext'=>'jpg,png,gif'
-            ])->rule('md5')->move('../uploads');
-            //这里采用MD5来给文件重命名，前两个字符是文件夹名称。
-            if($info){
-                echo $info->getSaveName();
-            }else{
-                echo $file->getError();
-            }
-            return'';
-        }else{
-            return $this->fetch();
-        }
-    }
+
     public function login(){
 
         return $this->fetch();

f81014d8d7a9223f81b946fc279746fb4d8a5541
shiroo <xkq722@gmail.com>
2018-10-26 17:18:24 +0800

    ..

--- application/admin/controller/File.php
+++ application/admin/controller/File.php
@@ -9,11 +9,14 @@
 namespace app\admin\controller;
 
 
+use think\Db;
+
 class File extends Base
 {
     public function upload(){
         if(IS_POST){
             $file=$this->request->file('file');
+            $updateIP=$this->request->ip();
             if(empty($file)){
                 return '没有获取到上传文件。';
             }
@@ -24,12 +27,24 @@ class File extends Base
             //这里采用MD5来给文件重命名，前两个字符是文件夹名称。
             if($info){
                 //上传成功
-                echo $info->getSaveName();
-                return true;
+                $data=[
+                    'file_name'=>$info->getFilename(),
+                    'file_path'=>str_replace('\\','/',$info->getSaveName()),
+                    'file_create_time'=>time(),
+                    'file_md5'=>$info->hash('md5'),
+                    'file_exists'=>1,
+                    'file_upload_ip'=>$updateIP,
+                    'file_admin_id'=>1,
+                    'file_type'=>$info->getExtension(),
+                    'file_size'=>$info->getSize()
+                ];
+                Db::name('file')->insert($data);
+                var_dump($data);
+                //return true;
             }else{
                 //上传失败
                 echo $file->getError();
-                return false;
+                //return false;
             }
         }else{
             return $this->fetch();

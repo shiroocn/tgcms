2482535c71e49622be751a6101e661e46bf8a1be
shiroo <xkq722@gmail.com>
2019-01-23 14:15:10 +0800

    ..

--- application/admin/controller/Brand.php
+++ application/admin/controller/Brand.php
@@ -39,8 +39,7 @@ class Brand extends Base
             'brand_name'=>$postData['brand_name'],
             'brand_weixin'=>$postData['brand_weixin'],
             'brand_weixinqr_path'=>$postData['brand_weixinqr_path'],
-            'brand_icon_path'=>$postData['brand_icon_path'],
-            'brand_bdl_id'=>0
+            'brand_icon_path'=>$postData['brand_icon_path']
         ];
         try{
             $result=Db::name('brand')->insert($data);
@@ -76,8 +75,7 @@ class Brand extends Base
             'brand_name'=>$postData['brand_name'],
             'brand_weixin'=>$postData['brand_weixin'],
             'brand_weixinqr_path'=>$postData['brand_weixinqr_path'],
-            'brand_icon_path'=>$postData['brand_icon_path'],
-            'brand_bdl_id'=>0
+            'brand_icon_path'=>$postData['brand_icon_path']
         ];
         try{
             $result=Db::name('brand')->where('brand_id',$brandID)->update($data);
@@ -90,26 +88,78 @@ class Brand extends Base
             return json_shiroo('edit.error');
         }
     }
-    public function addDefine(){
+    public function define(){
+        $postData=$this->request->param();
         if(IS_POST){
-            $postData=$this->request->param();
-            //进行数据校验
-
-            $data=[
-                'user_define_name'=>$postData['define_name'],
-                'user_define_note'=>$postData['define_note']
-            ];
-            $result=Db::name('user_define')->insert($data);
-            if($result){
-                $this->success('添加成功。');
-            }else{
-                $this->error('添加失败');
+            $page = $postData['page'] - 1;
+            $limit = $postData['limit'];
+            try {
+                $result = Db::name('brand_define')
+                    ->limit($page * $limit, $limit)
+                    ->order('bd_id', 'asc')
+                    ->select();
+                $count = Db::name('brand_define')->count('bd_id');
+            } catch (\Exception $e) {
+                return json_shiroo('database');
             }
+            return json_shiroo(0, '', $count ?: 0, $result);
+
         }else{
             return $this->fetch();
         }
     }
-    public function showDefine(){
+    public function defineAdd(){
+        $postData=$this->request->param();
+
+        $data=[
+            'bd_name'=>$postData['bd_name'],
+            'bd_note'=>$postData['bd_note'],
+        ];
+        try{
+            $result=Db::name('brand_define')->insert($data);
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('add.success');
+        }else{
+            return json_shiroo('add.error');
+        }
+    }
+    public function defineEdit(){
+        $postData=$this->request->param();
+
+        $bdID=$postData['bd_id'];
+        $data=[
+            'bd_name'=>$postData['bd_name'],
+            'bd_note'=>$postData['bd_note'],
+        ];
+        try{
+            $result=Db::name('brand_define')->where('bd_id',$bdID)->update($data);
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('edit.success');
+        }else{
+            return json_shiroo('edit.error');
+        }
+    }
+    public function defineDel(){
+        $postData=$this->request->param();
+        $bdID=$postData['bd_id'];
+        try{
+            $result=Db::name('brand_define')->where('bd_id',$bdID)->delete();
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('del.success');
+        }else{
+            return json_shiroo('del.error');
+        }
+    }
+    public function defineList(){
         $postData=$this->request->param();
         $brandID=$postData['brand_id'];
         if(IS_POST){
@@ -117,11 +167,11 @@ class Brand extends Base
             $limit = $postData['limit'];
             try {
                 $result = Db::name('brand_define_list')
-                    ->join('brand', 'brand_bdl_id=bdl_id')
+                    ->join('brand', 'brand_id=bdl_brand_id')
                     ->join('brand_define', 'bd_id=bdl_define_id')
                     ->where('bdl_brand_id', $brandID)
                     ->limit($page * $limit, $limit)
-                    ->order('page_id', 'asc')
+                    ->order('bdl_id', 'asc')
                     ->select();
                 $count = Db::name('brand_define_list')->where('bdl_brand_id', $brandID)->count('bdl_id');
             } catch (\Exception $e) {
@@ -130,7 +180,83 @@ class Brand extends Base
             return json_shiroo(0, '', $count ?: 0, $result);
 
         }else{
+            //获取页面上新增与编辑的参数名称列表，供用户选择
+            try{
+                $defines=Db::name('brand_define')->select();
+            }catch (\Exception $e){
+                $defines=array();
+            }
+
+            $this->assign('defines',$defines);
+            $this->assign('brand_id',$brandID);
             return $this->fetch();
+        }
+    }
+    public function defineListAdd(){
+        $postData=$this->request->param();
+
+        $bdlDefineID=$postData['bdl_define_id'];
+        $bdlDefineVar=$postData['bdl_define_var'];
+        $bdlBrandID=$postData['bdl_brand_id'];
+        $data=[
+            'bdl_define_id'=>$bdlDefineID,
+            'bdl_define_var'=>$bdlDefineVar,
+            'bdl_brand_id'=>$bdlBrandID
+        ];
+        $where=[
+            'bdl_define_id'=>$bdlDefineID,
+            'bdl_brand_id'=>$bdlBrandID
+        ];
+        try{
+            $exist=Db::name('brand_define_list')->where($where)->find();
+            if(!is_null($exist)){
+                return json_shiroo('exist');
+            }
+            $result=Db::name('brand_define_list')->insert($data);
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('add.success');
+        }else{
+            return json_shiroo('add.error');
+        }
+    }
+    public function defineListEdit(){
+        $postData=$this->request->param();
+
+        $bdlID=$postData['bdl_id'];
+        $bdlBrandID=$postData['bdl_brand_id'];
+        $data=[
+            'bdl_define_var'=>$postData['bdl_define_var'],
+        ];
+        $where=[
+            'bdl_id'=>$bdlID,
+            'bdl_brand_id'=>$bdlBrandID
+        ];
+        try{
+            $result=Db::name('brand_define_list')->where($where)->update($data);
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('edit.success');
+        }else{
+            return json_shiroo('edit.error');
+        }
+    }
+    public function defineListDel(){
+        $postData=$this->request->param();
+        $bdlID=$postData['bdl_id'];
+        try{
+            $result=Db::name('brand_define_list')->where('bdl_id',$bdlID)->delete();
+        }catch (\Exception $e){
+            return json_shiroo('database');
+        }
+        if($result>0){
+            return json_shiroo('del.success');
+        }else{
+            return json_shiroo('del.error');
         }
     }
 

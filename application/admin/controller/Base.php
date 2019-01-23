989c4485b46dc55d6f5f75d9f7d5d4be82412c35
shiroo <xkq722@gmail.com>
2019-01-23 21:56:12 +0800

    ..

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -14,6 +14,22 @@ class Base extends Controller
 {
     public function initialize(){
         define('IS_POST',$this->request->isPost()?:false);
+        define('ACTION',$action=$this->request->action()?:'');
+        echo ACTION;
+
+
+        //过滤不需要登陆的行为
+
+        /*if (!in_array($action, array('login', 'vertify'))) {
+            if (session('admin_id') > 0) {
+                $this->check_priv();//检查管理员菜单操作权限
+                $this->admin_id = session('admin_id');
+            }else {
+                (ACTION_NAME == 'index') && $this->redirect( U('Admin/Admin/login'));
+                $this->error('请先登录', U('Admin/Admin/login'), null, 1);
+            }
+        }*/
 
     }
+
 }

67de8366e2bd2ca32dc91ff7017025cd8ac10df0
shiroo <xkq722@gmail.com>
2019-03-01 10:59:32 +0800

    ..

--- config/database.php
+++ config/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'        => 'tgcms',
     // 用户名
-    'username'        => '',
+    'username'        => 'xkq722',
     // 密码
-    'password'        => '',
+    'password'        => 'xkq*147852',
     // 端口
     'hostport'        => '',
     // 连接dsn

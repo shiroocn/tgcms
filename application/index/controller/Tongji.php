84af66aca489ad6b9c818748f2016e585dddbf48
shiroo <xkq722@gmail.com>
2019-03-22 12:59:17 +0800

    ..

---
+++ application/index/controller/Tongji.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xkq72
+ * Date: 2019/3/21
+ * Time: 21:17
+ */
+
+namespace app\index\controller;
+
+
+use think\Db;
+use think\Exception;
+
+class Tongji extends Base
+{
+    public function add(){
+        $postData=$this->request->param();
+        dump($this->request->ip());
+
+
+        $data=[
+            'tj_keyword'=>'',
+            'tj_copy_text'=>'',
+            'tj_btn_text'=>'',
+            'tj_domain'=>'',
+            'tj_page_name'=>'',
+            'tj_create_data'=>'',
+            'tj_url'=>'',
+            'tj_device'=>'',
+            'tj_mac'=>'',
+            'tj_ip'=>''
+        ];
+
+        $where=[
+            'tj_mac'=>'',
+            'tj_ip'=>''
+        ];
+        /*try{
+            $tjDB=Db::name('tongji')->where($where)->find();
+        }catch (\Exception $exception){
+
+        }*/
+    }
+}
